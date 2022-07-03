f39a1ff14948b715196f6ba92efb4ca0c96caacf
AnnaRabychina <nutochka@tut.by>
2022-07-04 00:28:18 +0300

    feat: configure apollo-server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import { ApolloServer } from 'apollo-server';
 import 'dotenv/config';
+import { typeDefs } from './schema.js';
 
 const PORT = process.env.PORT || 3000;
+const server = new ApolloServer({ typeDefs });
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
