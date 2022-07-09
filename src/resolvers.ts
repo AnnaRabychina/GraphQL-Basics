87caabda4f394ea0cf792d38ca97d3dc32129747
AnnaRabychina <nutochka@tut.by>
2022-07-09 17:45:26 +0300

    feat: add resolvers and services for module of genres

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,10 +1,13 @@
 import { userResolver } from './modules/users/resolvers/users.resolver.js';
+import { genreResolver } from './modules/genres/resolvers/genres.resolver.js';
 
 export const appResolvers = {
   Query: {
     ...userResolver.Query,
+    ...genreResolver.Query
   },
   Mutation: {
     ...userResolver.Mutation,
+    ...genreResolver.Mutation
   }
 }
--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,7 @@
 import { userService } from './modules/users/services/users.service.js';
+import { genreService } from './modules/genres/services/genres.service.js';
 
 export const appServices = {
     userService: new userService(),
+    genreService: new genreService()
   }
